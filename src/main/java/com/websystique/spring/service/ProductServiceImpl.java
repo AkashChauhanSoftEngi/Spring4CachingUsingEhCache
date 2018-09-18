3a7cebfbd4650f46a66ca6f43993e51ed325f030
Akash Chauhan <akashchauhan.sejapan@gmail.com>
2018-09-18 11:51:13 +0530

    Add files via upload

---
+++ src/main/java/com/websystique/spring/service/ProductServiceImpl.java
@@ -0,0 +1,33 @@
+package com.websystique.spring.service;
+
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import com.websystique.spring.model.Product;
+
+@Service("productService")
+public class ProductServiceImpl implements ProductService{
+
+	@Override
+	@Cacheable("products")
+	public Product getByName(String name) {
+		System.out.println("Will print, First Time Only!");
+		slowLookupOperation();
+		return new Product(name,100);
+	}
+
+	@CacheEvict(value = "products", allEntries = true)
+	public void refreshAllProducts() {
+		//This method will remove all 'products' from cache, say as a result of flush API.
+	}	
+	
+	public void slowLookupOperation(){
+		 try {
+	            long time = 5000L;
+	            Thread.sleep(time);
+	        } catch (InterruptedException e) {
+	            throw new IllegalStateException(e);
+	       }
+	}
+}
